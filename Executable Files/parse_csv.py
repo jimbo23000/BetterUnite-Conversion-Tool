22c44ee508e3130f1ce9c1095005ca4ee7561170
jimbo23000 <antonio.ramos23@yahoo.com>
2024-03-11 19:42:46 -0500

    Added csv copying based off env variables

--- Executable Files/parse_csv.py
+++ Executable Files/parse_csv.py
@@ -3,21 +3,22 @@ import shutil
 from dotenv import load_dotenv
 import os
 
+# Creates output CSV, which will be altered using a writer object
+def create_output_csv(csv_src_path, csv_dst_path):
+    shutil.copyfile(csv_src_path, csv_dst_path)
+    
 # Utility function for debugging
 def print_headers(csv_path):
     with open(csv_path, newline='') as csvfile:
-        headers = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for column in headers:
-            print('\n'.join(column))
-
-# Creates output CSV, which will be altered using a writer object
-def create_output_csv(csv_path):
-    shutil.copyfile(csv_path, 'Donor_Information_Executable/Output CSVs/Donor Information.csv')
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            print('\n'.join(row))
 
 def main():
     load_dotenv()
-    csv_path = os.environ.get('CSV_PATH')
-    print_headers(csv_path)
-    #create_output_csv(csv_path)
+    csv_src_path = os.environ.get('CSV_SRC_PATH')
+    csv_dst_path = os.environ.get('CSV_DST_PATH')
+    create_output_csv(csv_src_path, csv_dst_path)
+    print_headers(csv_src_path)
 
 main()
