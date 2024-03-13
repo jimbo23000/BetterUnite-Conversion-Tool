2c784a73e6cdfc17b1d0a4d01fffda3b2499bf05
jimbo23000 <antonio.ramos23@yahoo.com>
2024-03-12 21:20:03 -0500

    Decreased the amount of referenced variables during initialization

--- Executable Files/display_ui.py
+++ Executable Files/display_ui.py
@@ -6,24 +6,33 @@ from parse_csv import field_names
 class GUI:
     def __init__(self):
         # Main menu portion
-        self.root = Tk(className=' Donor Information')
-        self.mm_frame = ttk.Frame(self.root, padding=10)
-        self.mm_frame.grid()
-        self.mm_dm_button = ttk.Button(self.mm_frame, text="Better Unite Template", padding=10, command=lambda: GUI.mm_dm_button_clicked(self))
-        self.mm_dm_button.grid(row=0, column=0)
+        root = Tk(className=' Donor Information')
+        mm_frame = ttk.Frame(root, padding=10)
+        mm_frame.grid()
+        mm_dm_button = ttk.Button(mm_frame, text="Better Unite Template", padding=10, command=lambda: GUI.mm_dm_button_clicked(self))
+        mm_dm_button.grid(row=0, column=0)
         # Donor menu portion
-        self.dm_frame = ttk.Frame(self.root, padding=10)
-        self.dm_label_text_var = field_names.pop()
-        self.dm_label = ttk.Label(self.dm_frame, text=self.dm_label_text_var, padding=10)
-        self.dm_label.grid(row=0, column=0)
-        self.dm_entry = ttk.Entry(self.dm_frame)
-        self.dm_entry.grid(row=0, column=1)
-        self.dm_mm_button = ttk.Button(self.dm_frame, text="Back to Main Menu", padding=10, command=lambda: GUI.dm_mm_button_clicked(self))
-        self.dm_mm_button.grid(row=1, column=0)
+        dm_frame = ttk.Frame(root, padding=10)
+        dm_label_text_var = field_names.pop()
+        dm_label = ttk.Label(dm_frame, text=dm_label_text_var, padding=10)
+        dm_label.grid(row=0, column=0)
+        dm_entry = ttk.Entry(dm_frame)
+        dm_entry.grid(row=0, column=1)
+        dm_mm_button = ttk.Button(dm_frame, text="Back to Main Menu", padding=10, command=lambda: GUI.dm_mm_button_clicked(self))
+        dm_mm_button.grid(row=1, column=0)
 
-        self.root.bind('<Return>', GUI.return_key_pressed)
+        root.bind('<Return>', GUI.return_key_pressed)
+        
+        self.root = root
+        self.mm_frame = mm_frame
+        self.mm_dm_button = mm_dm_button
+        self.dm_frame = dm_frame
+        self.dm_label_text_var = dm_label_text_var
+        self.dm_label = dm_label
+        self.dm_entry = dm_entry
+        self.dm_mm_button = dm_mm_button
         # Begin displaying GUI
-        self.root.mainloop()
+        root.mainloop()
 
     def mm_dm_button_clicked(self):
         self.mm_frame.grid_remove()
